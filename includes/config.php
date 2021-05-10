c824cc6c071ef8e7809fe99a181ced8663f77159
rayguang6 <83267994+rayguang6@users.noreply.github.com>
2021-05-10 19:28:33 +0800

    remote

--- includes/config.php
+++ includes/config.php
@@ -1,13 +1,14 @@
 <?php
 	ob_start();
 	session_start();
-	$timezone = date_default_timezone_set("Asia/Kuala_Lumpur");
+	//$timezone = date_default_timezone_set("Asia/Kuala_Lumpur");
 
-	$con = mysqli_connect("localhost","root","root","suddenly");
 
+	//$con = mysqli_connect("localhost","root","root","suddenly");
+	$con = mysqli_connect("remotemysql.com","n4NDrbQHNz ","7UWqSLB21t","n4NDrbQHNz ");
 	//check connection
 	if(mysqli_connect_errno()){
-		echo "leiguang fail sql connect";
+		echo "leiguang fail sql connect haishhhh";
 		echo "Fail to connect: ",mysqli_connect_errno();
 	}
 
