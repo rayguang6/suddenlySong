7f18a442baa285f1315b06e6803f2763ed802742
rayguang6 <83267994+rayguang6@users.noreply.github.com>
2021-05-10 19:38:58 +0800

    Add files via upload

--- includes/config.php
+++ includes/config.php
@@ -1,11 +1,28 @@
 <?php
 	ob_start();
 	session_start();
-	//$timezone = date_default_timezone_set("Asia/Kuala_Lumpur");
+	$timezone = date_default_timezone_set("Asia/Kuala_Lumpur");
+
+	//try remote
+	$host = 'remotemysql.com';
+	$db = 'n4NDrbQHNz';
+	$user = 'n4NDrbQHNz';
+	$pass = '7UWqSLB21t';
+	$charset = 'utf8mb4';
+
+	$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+	try{
+		$con = new PDO($dsn, $user, $pass);
+		$con->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+	} catch(PDOException $e){
+		echo "Fail to connect: ";
+	}
 
 
 	//$con = mysqli_connect("localhost","root","root","suddenly");
-	$con = mysqli_connect("remotemysql.com","n4NDrbQHNz ","7UWqSLB21t","n4NDrbQHNz ");
+	//$con = mysqli_connect("remotemysql.com","n4NDrbQHNz ","7UWqSLB21t","n4NDrbQHNz ");
+
 	//check connection
 	if(mysqli_connect_errno()){
 		echo "leiguang fail sql connect haishhhh";
